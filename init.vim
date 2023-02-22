530a85a06e6ab32191ab6ab02b320307b3e78ad5
Mark Tran <tkan145@gmail.com>
2023-02-22 21:13:43 +1000

    Fix more deprecated errors

--- init.vim
+++ init.vim
@@ -5,6 +5,6 @@ source ~/.vimrc
 lua require("lsp_config")
 
 " Import on save
-autocmd BufWritePost *.go lua vim.lsp.buf.formatting()
+autocmd BufWritePost *.go lua vim.lsp.buf.format { async = true }
 autocmd BufWritePre *.go lua org_imports(1000)
 
--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -37,29 +37,42 @@ function org_imports(timeout_ms)
     vim.validate { context = { context, "t", true } }
 
     local params = vim.lsp.util.make_range_params()
-    params.context = context
+    params.context = {only = {"source.organizeImports"}}
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
+
+    -- ############## START OLD
+    -- params.context = context
 
     -- See the implementation of the textDocument/codeAction callback
     -- (lua/vim/lsp/handler.lua) for how to do this properly.
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
-    if not result or next(result) == nil then return end
-    local actions = result[1].result
-    if not actions then return end
-    local action = actions[1]
+    -- local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
+    -- if not result or next(result) == nil then return end
+    -- local actions = result[1].result
+    -- if not actions then return end
+    -- local action = actions[1]
 
     -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
     -- is a CodeAction, it can have either an edit, a command or both. Edits
     -- should be executed first.
-    if action.edit or type(action.command) == "table" then
-      if action.edit then
-        vim.lsp.util.apply_workspace_edit(action.edit)
-      end
-      if type(action.command) == "table" then
-        vim.lsp.buf.execute_command(action.command)
-      end
-    else
-      vim.lsp.buf.execute_command(action)
-    end
+    -- if action.edit or type(action.command) == "table" then
+      -- if action.edit then
+      --   vim.lsp.util.apply_workspace_edit(action.edit)
+      -- end
+      -- if type(action.command) == "table" then
+        -- vim.lsp.buf.execute_command(action.command)
+      -- end
+    -- else
+      -- vim.lsp.buf.execute_command(action)
+    -- end
+    -- ############## END OLD
 end
 
 local has_words_before = function()
@@ -114,8 +127,10 @@ cmp.setup {
 }
 
 -- nvim-cmp supports additional completion capabilities
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+-- Add additional capabilities supported by nvim-cmp
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
